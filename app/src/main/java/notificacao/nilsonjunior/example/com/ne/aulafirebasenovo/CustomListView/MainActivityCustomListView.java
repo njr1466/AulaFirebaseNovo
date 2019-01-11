fe7a4efe7d312c7e5c115045286568bdee4feb5f
NIlson <njr1466@gmail.comm>
2019-01-11 09:00:24 -0300

    segundo Commit

--- app/src/main/java/notificacao/nilsonjunior/example/com/ne/aulafirebasenovo/CustomListView/MainActivityCustomListView.java
+++ app/src/main/java/notificacao/nilsonjunior/example/com/ne/aulafirebasenovo/CustomListView/MainActivityCustomListView.java
@@ -11,18 +11,7 @@ public class MainActivityCustomListView extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_custom_list_view);
-//        List<Aluno> alunos = todosOsCursos();
-//
-//        ListView listaDeCursos = (ListView) findViewById(R.id.listviewId);
-//
-//        //chamada da implementaçao do android:
-//        //ArrayAdapter<Curso> adapter = new ArrayAdapter<Curso>(this,
-//        //android.R.layout.simple_list_item_1, cursos);
-//
-//        //chamada da nossa implementação
-//        AdpaterAlunosPersonalizados adapter = new AdpaterAlunosPersonalizados(alunos, this);
-//
-//        listaDeCursos.setAdapter(adapter);
+
 
     }
 }
